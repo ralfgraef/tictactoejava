c9b48ad881bd41cfd8c1b1c62bc99a6089ef7c16
ralfgraef <graef.ralf@gmail.com>
2022-09-15 22:20:49 +0200

    add JFrame, titel_panel and button_panel

--- TicTacToe/src/com/ralfgraef/TicTacToe.java
+++ TicTacToe/src/com/ralfgraef/TicTacToe.java
@@ -1,9 +1,91 @@
 package com.ralfgraef;
 
-public class TicTacToe {
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class TicTacToe implements ActionListener{
+
+	Random random = new Random();
+	JFrame frame = new JFrame();
+	JPanel titel_panel = new JPanel();
+	JPanel button_panel = new JPanel();
+	JLabel textfield = new JLabel();
+	JButton[] buttons = new JButton[9];
+	boolean player1_turn;
+	
 	public TicTacToe() {
-		System.out.println("Hello TicTacToe");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800, 800);
+		frame.getContentPane().setBackground(new Color(50,50,50));
+		frame.setLayout(new BorderLayout());
+		frame.setTitle("Ralfs Tic-Tac-Toe");
+		frame.setVisible(true);
+		
+		textfield.setBackground(new Color(25,25,25));
+		textfield.setForeground(new Color(25,255,0));
+		textfield.setFont(new Font("Ink Free", Font.BOLD,65));
+		textfield.setHorizontalAlignment(JLabel.CENTER);
+		textfield.setText("Ralfs Tic-Tac-Toe");
+		textfield.setBorder(new EmptyBorder(25,0,0,0));
+		
+		textfield.setOpaque(true);
+		
+		titel_panel.setLayout(new BorderLayout());
+		titel_panel.setBounds(0,0,800,120);
+		
+		button_panel.setLayout(new GridLayout(3,3));
+		button_panel.setBackground(new Color(150,150,150));
+		
+		for (int i = 0; i < buttons.length; i++) {
+			buttons[i] = new JButton();
+			button_panel.add(buttons[i]);
+			buttons[i].setFont(new Font("MV Boli", Font.BOLD,120));
+			buttons[i].setFocusable(false);
+			buttons[i].addActionListener(this);
+		}
+		
+		titel_panel.add(textfield);
+		frame.add(titel_panel, BorderLayout.NORTH);
+		frame.add(button_panel);
+		
+		firstTurn();
 	}
+
+	private void firstTurn() {
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if (random.nextInt(2)==0) {
+			player1_turn = true;	
+			textfield.setText("X ist am Zug!");
+		} else {
+			player1_turn = false;	
+			textfield.setText("O ist am Zug!");
+		}
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 
 }
